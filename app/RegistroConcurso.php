d6ea18f1a4ca07b6d8c618997268d3c720e96ee0
CRISTIAN01AVALOS <cristianavalos.ed@gmail.com>
2023-02-17 21:17:58 -0600

    update bd pcete

--- app/RegistroConcurso.php
+++ app/RegistroConcurso.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistroConcurso extends Model
 {
-    protected $primaryKey = 'id_registro_concurso';
+    // protected $primaryKey = 'id_registro_concurso';
 
-    protected $table = 'registro_concurso';
+    // protected $table = 'registro_concurso';
+    // protected $fillable = [];
+
+    protected $connection = "pcete";
+    protected $table = 'cdvs_registro_concurso';
     protected $fillable = [];
 
 }
