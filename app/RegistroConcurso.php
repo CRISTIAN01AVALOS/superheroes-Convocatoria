c000d74da11a4a5bb703cc3ab59efdc66389eb15
aidarangel <aida.rangel@set.edu.mx>
2023-04-04 14:11:16 -0600

    Subir cambio Panel Admin 22 Modelos 3_04_2023

--- app/RegistroConcurso.php
+++ app/RegistroConcurso.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistroConcurso extends Model
 {
-    protected $primaryKey = 'id_registro_concurso';
+    // protected $primaryKey = 'id_registro_concurso';
 
     // protected $table = 'registro_concurso';
 
